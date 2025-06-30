06eb348cb1fdbfcde00f9eba9da08223d1a1f755
Gitansh Manchanda <manchandagitansh4@gmail.com>
2025-07-01 00:05:34 +0530

    feat: Add singleton class to manage socket and user on ws server

---
+++ apps/ws-backend/src/SocketManager.ts
@@ -0,0 +1,157 @@
+import WebSocket from "ws";
+import http from "http";
+import cookie from "cookie";
+import { CustomError } from "@gtdraw/common/utils/CustomError";
+import { prisma, User } from "@gtdraw/db";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { ACCESS_TOKEN_SECRET } from "@gtdraw/common/config";
+import { UserManager } from "./UserManager";
+export class SocketManager {
+  private static instance: SocketManager;
+  private ws: WebSocket;
+  private request: http.IncomingMessage;
+  private user: UserManager;
+  private constructor(ws: WebSocket, request: http.IncomingMessage) {
+    this.ws = ws;
+    this.request = request;
+    this.user = UserManager.getInstance();
+    this.handleEmitMessage = this.handleEmitMessage.bind(this); // binds context of this to class when we pass reference to this.handleEmitMessage otherwise context of this gets lost and gets resets to global this when ws lib calls this.handleEmitMessage.
+  }
+
+  public static getInstance(
+    ws: WebSocket,
+    request: http.IncomingMessage
+  ): SocketManager {
+    if (!SocketManager.instance) {
+      SocketManager.instance = new SocketManager(ws, request);
+      return SocketManager.instance;
+    }
+    return SocketManager.instance;
+  }
+
+  public async verifyCookies(): Promise<User | null> {
+    try {
+      const accessToken = cookie.parse(
+        this.request.headers.cookie || ""
+      ).accessToken;
+      if (!accessToken) {
+        console.error(
+          "Cookies Are Required To Establish Web Socket Connection"
+        );
+        this.ws.close();
+        throw new CustomError(401, "Unauthorized Request");
+      }
+      const decodedTokenInfo = jwt.verify(
+        accessToken!,
+        ACCESS_TOKEN_SECRET!
+      ) as JwtPayload;
+      if (!decodedTokenInfo || !decodedTokenInfo.id) {
+        this.ws.close();
+        console.error(
+          "You have sent invalid access token in web socket connection"
+        );
+        throw new CustomError(401, "Invalid Access Token!");
+      }
+      const user = await prisma.user.findFirst({
+        where: {
+          id: decodedTokenInfo.id,
+        },
+      });
+      if (!user) {
+        this.ws.close();
+        console.error(
+          "id from Decoded Token can't be matched to any user in DB"
+        );
+        throw new CustomError(401, "Invalid Access Token!");
+      }
+      return user;
+    } catch (error) {
+      console.error(
+        "Error Occurred while verifying JWT From Cookies:\n",
+        error
+      );
+      throw new CustomError(500, "Error Occurred While Verifying JWT");
+    }
+  }
+
+  public checkUser(token: string): string | null {
+    try {
+      const decodedToken = jwt.verify(
+        token!,
+        ACCESS_TOKEN_SECRET!
+      ) as JwtPayload;
+      if (!decodedToken || !decodedToken.id) {
+        this.ws.close();
+        //TODO: throw custom Error
+        return null;
+      }
+      return decodedToken.id;
+    } catch (error) {
+      throw new CustomError(
+        500,
+        `Error Occurred while verifying JWT:\n ${error}`
+      );
+    }
+  }
+
+  public emitMessage() {
+    this.ws.on("message", this.handleEmitMessage);
+  }
+
+  public async handleEmitMessage(data: WebSocket.RawData) {
+    let jsonString: string = "";
+    if (Buffer.isBuffer(data)) {
+      jsonString = data.toString("utf-8");
+    } else if (data instanceof ArrayBuffer) {
+      jsonString = new TextDecoder("utf-8").decode(data);
+    } else if (Array.isArray(data)) {
+      jsonString = Buffer.concat(data).toString("utf-8");
+    }
+    const parsedData = JSON.parse(jsonString);
+    if (parsedData.type === "join_room") {
+      const ws = this.ws;
+      const userId = this.user.getUserId(ws);
+      if (!userId) {
+        console.error(`Got userId undefined from getUserId method`);
+        return;
+      }
+      await this.user.joinRoom(userId, parsedData.roomId);
+    }
+
+    if (parsedData.type === "leave_room") {
+      const userId = this.user.getUserId(this.ws);
+      if (!userId) {
+        console.error(`Got userId undefined from getUserId method`);
+        return;
+      }
+      await this.user.leaveRoom(userId, parsedData.roomId);
+    }
+
+    if (parsedData.type === "chat") {
+      const userId = this.user.getUserId(this.ws);
+      if (!userId) {
+        console.error(`Got userId undefined from getUserId method`);
+        return;
+      }
+      await this.user.sendMessage(
+        userId,
+        parsedData.roomId,
+        parsedData.message
+      );
+    }
+  }
+
+  public disconnet() {
+    this.ws.on("close", () => {
+      const userId = this.user.getUserId(this.ws);
+      const isUserRemoved = this.user.removeUser(userId!);
+      if (isUserRemoved) {
+        console.log(`User Disconnected`);
+        return;
+      } else {
+        console.log(`User doesn't exist while trying to disconnect`);
+        return;
+      }
+    });
+  }
+}
---
+++ apps/ws-backend/src/UserManager.ts
@@ -0,0 +1,170 @@
+import { CustomError } from "@gtdraw/common/utils/CustomError";
+import { prisma } from "@gtdraw/db";
+import WebSocket from "ws";
+export interface User {
+  ws: WebSocket;
+  rooms: Array<string>;
+}
+
+export class UserManager {
+  private static instance: UserManager;
+  private users: Map<string, User>;
+  private constructor() {
+    this.users = new Map<string, User>();
+  }
+
+  public static getInstance(): UserManager {
+    if (!UserManager.instance) {
+      UserManager.instance = new UserManager();
+      return UserManager.instance;
+    }
+    return UserManager.instance;
+  }
+
+  public getUserId(ws: WebSocket) {
+    for (let [userId, user] of this.users.entries()) {
+      if (user.ws === ws) {
+        return userId;
+      }
+    }
+  }
+
+  public addUser(userId: string, ws: WebSocket) {
+    if (!this.users.has(userId)) {
+      this.users.set(userId, { ws, rooms: [] });
+      console.log(`User with ${userId} added successfully!`);
+      return;
+    }
+    console.log(`User with ${userId} already exists `);
+    return;
+  }
+
+  public async joinRoom(userId: string, roomId: string) {
+    try {
+      if (!this.users.has(userId)) {
+        console.error(`User with ${userId} doesn't exist in users list`);
+        throw new CustomError(404, "User Doesn't Exist!");
+      }
+      //Check if room exists in DB
+      const room = await prisma.room.findFirst({
+        where: {
+          id: roomId,
+        },
+      });
+      if (!room) {
+        this.users.get(userId)?.ws.close();
+        console.error('Room with room id "roomId " Not Found in DB');
+        throw new CustomError(404, "Room Doesn't Exist!");
+      }
+      //Check if user has already joined the room if yes return, (helps to avoid duplicates)
+      if (this.users.get(userId)?.rooms.includes(roomId)) {
+        console.log(`User ${userId} is already added to room ${roomId}`);
+        return;
+      }
+      this.users.get(userId)?.rooms.push(roomId);
+      console.log(`User ${userId} added to room ${roomId}`);
+      return;
+    } catch (error) {
+      console.error(`Error Occurred while joining room:\n ${error}`);
+      throw new CustomError(500, `Error Occurred while joining room!`);
+    }
+  }
+  public async leaveRoom(userId: string, roomId: string) {
+    try {
+      if (!this.users.has(userId)) {
+        console.error(`User with ${userId} doesn't exist in users list`);
+        throw new CustomError(404, "User Doesn't Exist!");
+      }
+
+      //Check if room exists in DB
+      const room = await prisma.room.findFirst({
+        where: {
+          id: roomId,
+        },
+      });
+      if (!room) {
+        this.users.get(userId)?.ws.close();
+        console.error('Room with room id "roomId " Not Found in DB');
+        throw new CustomError(404, "Room Doesn't Exist!");
+      }
+
+      const ws = this.users.get(userId)?.ws;
+      const rooms = this.users
+        .get(userId)
+        ?.rooms.filter((room) => room !== roomId);
+
+      if (!ws) {
+        console.error(`User ${userId} ws is undefined`);
+        return;
+      }
+
+      if (!rooms) {
+        console.error(`User ${userId} rooms array is undefined`);
+        return;
+      }
+
+      this.users.set(userId, { ws, rooms });
+      console.log(`User ${userId} left room ${roomId}`);
+      return;
+    } catch (error) {
+      console.error(`Error Occurred while leaving room:\n${error}`);
+      throw new CustomError(500, "Error Occurred while leaving room!");
+    }
+  }
+  public async sendMessage(userId: string, roomId: string, message: string) {
+    try {
+      //check if user exists in users
+      if (!this.users.has(userId)) {
+        console.log(`User ${userId} Doesn't Exist in users list`);
+        throw new CustomError(404, "User Doesn't Exist");
+      }
+
+      //Check if room exists in DB
+      const room = await prisma.room.findFirst({
+        where: {
+          id: roomId,
+        },
+      });
+      if (!room) {
+        this.users.get(userId)?.ws.close();
+        console.error('Room with room id "roomId " Not Found in DB');
+        throw new CustomError(404, "Room Doesn't Exist!");
+      }
+
+      //Check if user is part of the room he is trying to send message to
+      if (!this.users.get(userId)?.rooms.includes(roomId)) {
+        console.error(`User ${userId} does not have ${roomId} in rooms array`);
+        throw new CustomError(401, "User is not part of the room!");
+      }
+
+      // Now, before broadcasting message in room save in DB
+      //TODO: Add queuing logic to reduce latency for broadcasting messages
+      const chat = await prisma.chat.create({
+        data: {
+          userId,
+          roomId,
+          message,
+        },
+      });
+      //Broadcast message to each user in room
+      this.users.forEach((user) => {
+        if (user.rooms.includes(roomId)) {
+          user.ws.send(
+            JSON.stringify({
+              type: "chat",
+              message,
+              roomId,
+            })
+          );
+        }
+      });
+    } catch (error) {
+      console.error(`Error Occured while sending message:\n ${error}`);
+      throw new CustomError(500, `Error Occurred while sending message!`);
+    }
+  }
+
+  public removeUser(userId: string) {
+    return this.users.delete(userId);
+  }
+}
